fde171a9d62907a46bb6a08151d4731daf18efda
AntonioNtV <antoniontv18@gmail.com>
2020-08-24 15:11:42 -0300

    Minor changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express'
+import createUser from './services/CreateUser'
 
 export function helloWorld(resquest: Request, response: Response) {
+    const user = createUser({
+        name: 'Antonio Neto',
+        email: 'antonio@mail.com',
+        password: '132456789'
+    })
     return response.json({ message: 'Hello World' })
 }
