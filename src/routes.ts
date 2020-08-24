61f1f861e66b5ed050874f2d2c94ef73d670339b
AntonioNtV <antoniontv18@gmail.com>
2020-08-24 15:16:41 -0300

    Adicionando tipagens de tech para o user

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,17 @@ export function helloWorld(resquest: Request, response: Response) {
     const user = createUser({
         name: 'Antonio Neto',
         email: 'antonio@mail.com',
-        password: '132456789'
+        password: '132456789',
+        techs: [
+            'TypeScript', 
+            'ReacJS', 
+            'Java', 
+            'React Native',
+            {
+                title: 'Javascript',
+                experience: 80
+            }
+        ]
     })
     return response.json({ message: 'Hello World' })
 }
